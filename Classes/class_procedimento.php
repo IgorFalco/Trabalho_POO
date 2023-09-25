cf5770d8acd9c68f680f164a0c09f0024aaf2e79
ptrindader <pedro.treis@yahoo.com.br>
2023-09-24 23:14:38 -0300

    add classe procedimento

--- Classes/class_procedimento.php
+++ Classes/class_procedimento.php
@@ -1,5 +1,48 @@
 <?php
+include_once('./class_orcamento.php');
 
-class Procedimento{
-    
+class procedimento{
+
+    protected $nome_proced;
+    protected $descricao_proced;
+    protected $valor_proced;
+
+    public function _construct(string $nome_proced, string $descricao_proced, int $valor_proced){
+
+        $this->nome_proced = $nome_proced;
+        $this->descricao_proced = $descricao_proced;
+        $this->valor_proced = $valor_proced;
+    }
+
+
+
+    //getters
+    public function getNomeProced(){
+        return $this->nome_proced;
+    }
+
+    public function getDescricaoProced(){
+        return $this->descricao_proced;
+    }
+
+    public function getValorProced(){
+        return $this->valor_proced;
+    }
+
+
+
+    //setters
+    public function setNomeProced($nome_proced){
+        $this->nome_proced = $nome_proced;
+    }
+
+    public function setDescricaoProced($descricao_proced){
+        $this->descricao_proced = $descricao_proced;
+    }
+
+    public function setValorProced($valor_proced){
+        $this->valor_proced = $valor_proced;
+    }
+
+
 }
