aeadc1c4a4b1090650b7498da0a1d864510852a2
Gabriel Cançado <gabriel.cancadoc@gmail.com>
2023-12-02 18:50:07 -0300

    Pessoa, Dentista Parceiro e Celetista
    A comissão do Dentista Parceiro é definida por Especialidade(Sprint 5). Removemos
    o atributo comissão do Dentista Parceiro e adicionamos uma porcentagem no vetor de
    especialidades da classe Dentista Pai. Agora não existe diferença entre Densita Parceiro
    e Dentista Pai.
    
    Adicionamos gets e sets em Pessoa

--- Classes/class_DentistaParceiro.php
+++ Classes/class_DentistaParceiro.php
@@ -1,36 +1,13 @@
 <?php
 
-include_once('global.php');
+include_once('./global.php');
 
 class DentistaParceiro extends Dentista
 {
 
-  protected $comissao;
-  protected $percComissao;
-
-  public function __construct(string $nome, string $email, string $telefone, string $cpf, string $cro, Especialidades $especialidade, float $percComissao, string $logradouro, string $numero, string $bairro, string $cidade, string $estado)
+  public function __construct(string $nome, string $email, string $telefone, string $cpf, string $cro, Especialidades $especialidade, float $porcentagem_, string $logradouro, string $numero, string $bairro, string $cidade, string $estado)
   {
-    parent::__construct($nome, $email, $telefone, $cpf, $cro, $especialidade, $logradouro, $numero, $bairro, $cidade, $estado);
-    $this->percComissao = $percComissao;
+    parent::__construct($nome, $email, $telefone, $cpf, $cro, $especialidade, $porcentagem_, $logradouro, $numero, $bairro, $cidade, $estado);
   }
 
-  public function getComissao(): float
-  {
-    return $this->comissao;
-  }
-
-  public function getPercComissao(): float
-  {
-    return $this->percComissao;
-  }
-
-  public function addComissao(float $nova_comissao)
-  {
-    $this->comissao += $this->percComissao * $nova_comissao;
-  }
-
-  public function resetComissao()
-  {
-    $this->comissao = 0.0;
-  }
 }
